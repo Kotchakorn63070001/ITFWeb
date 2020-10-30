dc8e14ffa4c6893d4c64f2b31183ff694854389f
Kotchakorn63070001 <73680019+Kotchakorn63070001@users.noreply.github.com>
2020-10-30 10:54:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-?>
+</html>
+    <head>
+        <title>Hello</title>
+    </head>
+    <body>
+        <h1>Profile</h1>
+        <p>ชื่อ-นามสกุล : นางสาวกชกร เขจรไชย</p>
+        <p>ชื่อเล่น : เจน</p>
+        <p>สัตว์เลี้ยงที่ชอบ : บีเกิ้ล ซามอยด์</p>
+        <img src="beagle.jpg">
+        <br>
+        <img src="samoyed.jpg">
+        <br>
+        <p>เพลงที่ชอบ : Daybreak</p>
+        <a href="https://www.youtube.com/watch?v=bBnWklKfYN0">This is a Link</a>
+        <p>เล่าเรื่องตลก เรื่อง ตลกมั้ย??</p>
+        <p>5555555555555555555555 ไม่ตลก</p>
+    
+    </body>
+</html>
