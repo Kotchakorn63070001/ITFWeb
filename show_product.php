de8f4289175dba24d037cf9a51f3f558c2c40ae6
Kotchakorn63070001 <73680019+Kotchakorn63070001@users.noreply.github.com>
2020-11-27 10:53:41 +0700

    Update show_product.php

--- show_product.php
+++ show_product.php
@@ -34,7 +34,7 @@ $res = mysqli_query($conn, 'SELECT * FROM price');
                   <td style="background-color: #ffffff;"><?php echo $Result['Amount'];?></td>
                   <td style="background-color: #ffffff;"><?php echo $Result['Total'];?></td>
                   <td style="background-color: #ffffff;">
-                      <a href="delete_form.php?id=<?=$Result['id'];?>" class="btn btn-info">delete</a>
+                      <a href="delete_form.php?Id=<?=$Result['Id'];?>" class="btn btn-info">delete</a>
                   </td>
                 </tr>
               <?php
@@ -54,3 +54,4 @@ mysqli_close($conn);
 ?>
 </body>
 </html>
+
